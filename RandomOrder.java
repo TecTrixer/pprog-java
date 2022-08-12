d4adaeab5cb974abd15790da06d168cef41dfa86
Constantin Mussaeus <cmussaeus@student.ethz.ch>
2022-08-12 16:03:31 +0200

    added RandomOrder.java

---
+++ RandomOrder.java
@@ -0,0 +1,43 @@
+public class RandomOrder {
+    public static void main(String[] args) {
+        int iterations = 10;
+
+        // Creating both threads and letting them execute in parallel.
+        Thread t1 = new AddThread(iterations, 1);
+        Thread t2 = new AddThread(iterations, 2);
+        
+        // Starting both threads, here the order isn't well defined anymore.
+        t1.start();
+        t2.start();
+
+        // Waiting for both to finish.
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            System.out.println("Error whilst joining threads.");
+        }
+
+        System.out.println("Both threads finished.");
+    }
+}
+
+// Thread which will print if its currently running.
+class AddThread extends Thread {
+    int iterations;
+    int id;
+
+    // Class constructor
+    public AddThread(int iterations, int id) {
+        this.iterations = iterations;
+        this.id = id;
+    }
+
+    // Main running method
+    public void run() {
+        for (int i = 0; i < this.iterations; i++) {
+            // Printing that its currently running, this locks on the stdout object.
+            System.out.println("Thread " + this.id + " is currently running.");
+        }
+    }
+}
